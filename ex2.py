71ae197a987c77da0a4340b18a984fd765516554
Ben Carter <32357076+Ellemelmarta@users.noreply.github.com>
2017-10-04 22:32:22 +0100

    Add files via upload
    This commit is my work with testing python commands such as: Printing with speech marks and ' in file ex1. In ex2 I tested math commands and worked out what each done. Ex3 was using variables and understanding how exactly they work. Finally ex4 was me using variables and changing them around as well as using math signs to convert the variables from KG to Pound or CM to Inches.

---
+++ ex2.py
@@ -0,0 +1,34 @@
+print("These are how many animals I have:")
+
+print("Cows", 15+30/6)
+#The + sign is doing addition and the / sign is doing division.
+print("Sheep", 100-30*3%4)
+#The - sign is doing subtraction and the * sign is doing multiplication.
+print("Now I will count my plants:")
+
+print(3+2+1-5+4%2-1/4+6)
+#Calculation for plants.
+print("Is it true that 3+2<5-7?")
+print(3+2<5-7)
+#The < and > symbols are checking if the number is greater than or less than 'x'
+print("What is 3+2?", 3+2)
+print("What is 5-7?", 5-7)
+
+print("Can you see why that printed False earlier?")
+#Because 3+2 = less than 5-7
+print("Is it greater?", 5>-2)
+print("Is it greater or equal?", 5>=-2)
+print("Is it less or equal?", 5<=-2)
+#Printing true or false results for the above if they are greater and less than.
+#The symbols <= and => are greater than or equal to or less than or equal to.
+print("What is 3*2?")
+print(3*2)
+#Symbol * test to see what it does.
+print("What is 3 % 4?")
+print(3%4)
+#Symbol % test it divides and shows you the remainder of the division.
+#If the first number is smaller than the second number then it shows you the first number.
+print("Sheep calculation test of 90 % 4 = ")
+print(90%4)
+#Testing to see how the "Sheep" calculation is done.
+#"Sheep" calculation is done 30*3 and then it does 90%4 giving the answer of 2 which means 100-2=98.
