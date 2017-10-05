8b6542c5a54ac3d1fd06f8655f9cda3b3c26ae7a
CJace33 <s176047@UCS.ac.uk>
2017-10-05 11:19:33 +0100

    changed the age input so that if it isnt an int it prints 'you stupid' if it is an int it will print a message with your age stopping people from inputting a letter to an age input

--- ex6.py
+++ ex6.py
@@ -1,4 +1,8 @@
-name = input("What's your name? ")
-print("Nice to meet you " + name + ".")
-age = input("What's your age? ")
-print("Your age is " + age + ".")
+name = (input ("What's your name? "))
+print(f"Nice to meet you {name}.")
+
+age = input ("What's your age? ")
+if age == int:
+    print(f"Your age is {age}")
+else:
+    print("you stupid")
