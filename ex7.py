dc6e29bf60f48208d46b1e5f441a044d3507abbb
ellemelmarta <tractor1978@gmail.com>
2017-10-11 18:16:46 +0100

    Finished calculations so that they calculate the monthly interest properly now as well as adding new variables such as finalcost for working out what the cost is after the deposit they make.

--- ex7.py
+++ ex7.py
@@ -1,27 +1,25 @@
-portion_deposit = 0.2
-current_savings = 0
-#At the end of each month, your savings will be increased by the return of your investment,
-#plus a percentage of your monthly_salary
-r = 0.04
-monthlyr = current_savings*r/12
-
 annual_salary = float(input ("Please enter your annual salary: "))
-
-monthly_salary = annual_salary/12
-
 portion_saved = float(input ("Please enter the percentage of your salary you wish to save, as a decimal: "))
-
 total_cost = float(input ("What is the cost of your desired home: "))
 
+monthly_salary = annual_salary/12
+portion_deposit = 0.2*total_cost
+#calculating the deposit required to be put down
+current_savings = 0
+r = 0.04
 totalmonths = 0
+monthlyr = current_savings*r/12
+#monthly interest calculation
+finalcost = total_cost - portion_deposit
+#added finalcost variable to work out what the cost is without the deposit
 
-#monthly_salary * portion_saved + monthlyr + current_savings
-
-
-while current_savings < total_cost:
+while current_savings < finalcost:
     current_savings += (monthly_salary * portion_saved + monthlyr)
+#calculating the new current savings each month
+    monthlyr += current_savings*r/12
+#calculating the new monthlyr for next month this makes sure first month monthlyr is 0
     totalmonths += 1
-    print(f"{current_savings}")
+#adding 1 month to the total months value that will be printed
 else:
-    print(f"{totalmonths}")
-#this is gunna be used for working out actual monthly income
+    print(f"The amount of months you need is: {totalmonths}")
+#When the value of current_savings reaches what finalcost is then it will print this statement showing the count of loops as months
