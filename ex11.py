47cdcdfda2d003daeb849d307cf7912de505c249
ellemelmarta <tractor1978@gmail.com>
2017-10-24 14:27:46 +0100

    completed and working ex11.py changed the while statement by removing the last elif and replacing it with an else and not having it tabbed into the while loop code now works as intended.

--- ex11.py
+++ ex11.py
@@ -5,19 +5,18 @@ x = int(randint (1,100))
 #make user guess a number with input
 guess = int(input("Guess a number between 1 - 100: "))
 
-num_guesses = 0
+num_guesses = 1
 #tell them higher or lower
 while guess != x:
     if guess > x and guess != x:
         print ("wrong, too high")
         guess = int(input("Guess again: "))
         num_guesses += 1
+    #if statement to make sure the guess isnt equal to input and is higher than it to then tell the user
     if guess < x and guess != x:
         print ("wrong, too low")
         guess = int(input("Guess again: "))
         num_guesses += 1
-    elif guess == x:
-        print (f"congratulations you got it in {num_guesses} guesses. x = {x}")
-#track the number of guesses they do till correct
-#this doesnt even work how you want it to sometimes it canceles out early for some reason
-#try find a different way to do it
+    #if statement to make sure the guess isnt equal to input and is lower than it to then tell the user
+else:
+    print (f"Congratulations you got it in {num_guesses} guesses. Number = {x}")
